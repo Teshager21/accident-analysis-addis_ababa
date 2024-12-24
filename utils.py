b2f371166c9880ecd61f4a97d8b357730f11aa6e
Teshager <teshager8922@gmail.com>
2024-12-24 08:51:55 +0300

    futher look into attributes

--- utils.py
+++ utils.py
@@ -2,37 +2,43 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import pandas as pd
 
-def count_plot(df, feature, title, ordered=True, ax=None):  
-    plt.figure(figsize=(10,5))
-    # Set the axis for the plot
+def count_plot(df, feature, title, ordered=True, ax=None, hue=None, figsize=(10, 5)):
+    # If no Axes object is provided, create a new figure and axis with the specified size
     if ax is None:
-        ax = plt.gca()  # If no axis is provided, use the current axis
-    
-    # Order the categories based on value counts if 'ordered' is True
+        fig, ax = plt.subplots(figsize=figsize)
+        own_fig = True
+    else:
+        own_fig = False
+
+    # Determine the order of categories based on value counts if 'ordered' is True
     order = df[feature].value_counts().sort_values(ascending=False).index if ordered else None
-    
+
     # Create the countplot
-    sns.countplot(data=df, y=feature, palette='Dark2', order=order, ax=ax)
-    
+    sns.countplot(data=df, y=feature, palette='Dark2', order=order, ax=ax, hue=hue)
+
     # Total number of entries
     total = len(df)
-    
+
     # Set plot labels and title
     ax.set_xticklabels(ax.get_xticklabels(), fontsize=12)
     ax.set_yticklabels(ax.get_yticklabels(), fontsize=12)
     ax.set_title(title, fontsize=14)
     ax.set_xlabel('')
     ax.set_ylabel('')
-    
+
     # Add percentage annotations to each bar
     for p in ax.patches:
         width = p.get_width()  # Count of each category
         percentage = (width / total) * 100  # Calculate percentage
         ax.annotate(f'{percentage:.1f}%',
-                    (p.get_x() + p.get_width(), p.get_y() + p.get_height() / 2), 
+                    (p.get_x() + p.get_width(), p.get_y() + p.get_height() / 2),
                     ha='left', va='center', fontsize=12, color='black')
 
-    plt.tight_layout()
+    # Adjust layout and display the plot if it's a standalone figure
+    if own_fig:
+        plt.tight_layout()
+        plt.show()
+
 
 def cross_tab_features(df,feature1,feature2):
     cross_tab= pd.crosstab(index=df[feature1], columns=df[feature2], margins=True)
